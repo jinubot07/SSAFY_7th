11bb4f875689c6a9e97b5df27f4f5b78d4b39dc2
jinu <ehzndk@gmail.com>
2022-02-17 11:33:29 +0900

    youtubelive 0215 update

---
+++ java_algorithm/youtube_live/src/youtube0215/MeetingRoomGreedyTest.java
@@ -0,0 +1,21 @@
+package youtube0215;
+
+import java.util.Scanner;
+
+public class MeetingRoomGreedyTest {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int count = scann.nextInt();	// 회의 수
+		
+		for(int i=0; i<count; i++) {
+			
+		}
+	
+		// 회의 목록을 종료시간 기준으로 오름차순, 종료시간이 같다면 시작시간 기준으로 오름차순 정렬
+		
+		
+		
+	}
+	
+}
